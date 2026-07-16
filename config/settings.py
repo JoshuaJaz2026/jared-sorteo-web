3a6081cf83399736b78fe823c774d2f771a2b141
Joshua <jabarcap.2004@gmail.com>
2026-07-16 07:56:26 -0500

    Agregada la variable STATIC_ROOT para el despliegue en Render

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-o0q+jlx75#yz%@e-5&d_xcd&0-b3%wnc5y^8k1w(xmkdha^__l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,3 +128,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+# MAGIA PARA RENDER: La "maleta" donde se guardarán todos los estáticos en producción
+STATIC_ROOT = BASE_DIR / 'staticfiles'
