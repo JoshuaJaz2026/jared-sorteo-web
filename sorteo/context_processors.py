22672630255f1bcd26921f743d6aeba08482dee3
Joshua <jabarcap.2004@gmail.com>
2026-07-16 22:58:07 -0500

    Mejoras Elite del Panel de Administrador

--- sorteo/context_processors.py
+++ sorteo/context_processors.py
@@ -1,14 +1,18 @@
 from .models import Participante
 
 def metricas_sorteo(request):
-    # Calculamos los datos en tiempo real
+    # Contamos cuántos están marcados y desmarcados
     validados = Participante.objects.filter(participando=True).count()
     pendientes = Participante.objects.filter(participando=False).count()
-    recaudado = validados * 10
     
-    # Enviamos estas variables al diseño del panel
+    # Calculamos el dinero multiplicando por S/ 10.00
+    recaudado = validados * 10
+    dinero_pendiente = pendientes * 10
+    
+    # Enviamos todo al diseño
     return {
         'dash_validados': validados,
-        'dash_pendientes': pendientes,
         'dash_recaudado': recaudado,
+        'dash_pendientes': pendientes,
+        'dash_dinero_pendiente': dinero_pendiente,
     }
