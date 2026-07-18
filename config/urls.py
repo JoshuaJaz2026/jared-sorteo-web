4d76c87dbe813e061afcfba6281b8f4d7232d2d0
Joshua <jabarcap.2004@gmail.com>
2026-07-17 23:19:06 -0500

    Corregido error de importacion en urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from sorteo.views import guardar_participante, ver_boleto # Agregamos ver_boleto aquí
+from sorteo.views import landing_sorteo, ver_boleto
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', guardar_participante, name='landing'),
-    # NUEVA RUTA PARA EL BOLETO:
+    # Conectamos la ruta principal con la función correcta: landing_sorteo
+    path('', landing_sorteo, name='landing_sorteo'),
+    # Ruta para los tickets digitales
     path('boleto/<int:participante_id>/', ver_boleto, name='ver_boleto'),
 ]
